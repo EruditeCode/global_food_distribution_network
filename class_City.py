2df50238f316c2ba10f5c9ef2619529b9dd2506f
EruditeCode <erudite.home@gmail.com>
2023-02-08 12:59:57 +0000

    Added the functionality to show a label when the city was hovered over by the mouse.

--- class_City.py
+++ class_City.py
@@ -1,3 +1,4 @@
+import support_functions as sf
 
 class City:
 	def __init__(self, name, x, y):
@@ -5,3 +6,10 @@ class City:
 		self.pos = (x, y)
 		self.color = (255,255,255)
 		self.rad = 2
+
+	def update(self, mouse_pos, screen, font):
+		self.show_label_on_hover(mouse_pos, screen, font)
+
+	def show_label_on_hover(self, mouse_pos, screen, font):
+		if sf.euclidean_distance(mouse_pos, self.pos) < self.rad:
+			sf.draw_label(self.name, self.pos, screen, font)
